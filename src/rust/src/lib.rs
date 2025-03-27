cddf0173fa1cf9f443ee771c83ec591310ef6943
Anyll Markevich <anyllmarkevich@gmail.com>
2025-03-27 16:19:04 -0600

    Slimmed down the code to (mostly) the core currently functional code.

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -18,29 +18,6 @@ fn determ_pva(
 ) -> &'static str {
     let s_mat = survival_matrix.data();
     let p_mat = population_matrix.data();
-    //let population = ecolysis_core::populations::PvaDeterministicPopulation::build_from_vectors(
-    //    p_mat.to_vec(),
-    //    s_mat.to_vec().into_iter().map(|item| item.to_vec).collect(),
-    // Take the input dimensions
-    let dims = survival_matrix.dim();
-
-    // Get the number of rows and columns
-    let nrows = dims[0] as usize;
-    let ncols = dims[1] as usize;
-
-    // Create a new vector to hold the transposed data
-    let mut transposed_data = Vec::with_capacity(nrows * ncols);
-
-    // Transpose the matrix by swapping rows and columns
-    for row in 0..nrows {
-        for col in 0..ncols {
-            // Get a reference to the element at position (row, col)
-            let value = &survival_matrix[[col, row]];
-            // Collect the reference for the transposed matrix
-            transposed_data.push(value);
-        }
-    }
-    println!("{:?}", transposed_data);
     println!("\n");
     println!("{:?}", survival_matrix.data());
     println!("\n");

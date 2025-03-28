8424279701edab7346ca75deab5faf884e156d0d
Anyll Markevich <anyllmarkevich@gmail.com>
2025-03-28 02:32:45 -0600

    Deterministic population simulation is available as a function in R! Documentation is still needed, though.

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -11,20 +11,37 @@ fn hello_world() -> &'static str {
 /// IDK what this does
 /// @export
 #[extendr]
-fn determ_pva(survival_matrix: RMatrix<f64>, population_matrix: &[f64], generations: i32) {
+fn determ_pva(
+    survival_matrix: RMatrix<f64>,
+    population_matrix: &[f64],
+    generations: u32,
+) -> RMatrix<f64> {
     let ncol = survival_matrix.nrows();
     let s_vec = survival_matrix.data();
-    let p_vec = population_matrix;
+    let p_vec = population_matrix.to_vec();
     let s_mat: Vec<_> = s_vec
         .to_vec()
         .chunks_exact(ncol)
         .map(|col| col.to_vec())
         .collect();
-
-    println!("{:?}", s_mat);
-    println!("\n");
-    println!("{:?}", p_vec);
-    println!("\n");
+    let pop = PvaDeterministicPopulation::build_from_vectors(p_vec, s_mat)
+        .expect("Inputs are not formatted correctly.");
+    let output = pop.deterministic_projection(generations);
+    output.print_output();
+    let num_out = output.return_numerical_output();
+    let flattened = num_out
+        .iter()
+        .flat_map(|array| array.iter().cloned())
+        .collect();
+    RMatrix::new_matrix(
+        Vec::len(&flattened) / Vec::len(&num_out[0]),
+        Vec::len(&num_out[0]),
+        |r, c| num_out[r][c],
+    )
+    //println!("{:?}", s_mat);
+    //println!("\n");
+    //println!("{:?}", p_vec);
+    //println!("\n");
 }
 
 // Macro to generate exports.
